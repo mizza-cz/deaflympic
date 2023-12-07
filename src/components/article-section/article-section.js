37ce5fd551346dc6556a6e0bc42c48fb9f69899e
mizza-cz <mizza.cz666@gmail.com>
2023-12-07 07:02:41 +0100

    add pages

--- src/components/article-section/article-section.js
+++ src/components/article-section/article-section.js
@@ -1,10 +1,18 @@
-document.getElementById("copyLink").addEventListener("click", function () {
-  var linkToCopy = this.getAttribute("data-link");
-  var textarea = document.createElement("textarea");
-  textarea.value = linkToCopy;
-  document.body.appendChild(textarea);
-  textarea.select();
-  textarea.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  document.body.removeChild(textarea);
+document.addEventListener("DOMContentLoaded", function () {
+  var copyLinkButton = document.getElementById("copyLink");
+
+  if (copyLinkButton) {
+    copyLinkButton.addEventListener("click", function () {
+      var linkToCopy = this.getAttribute("data-link");
+      var textarea = document.createElement("textarea");
+      textarea.value = linkToCopy;
+      document.body.appendChild(textarea);
+      textarea.select();
+      textarea.setSelectionRange(0, 99999);
+      document.execCommand("copy");
+      document.body.removeChild(textarea);
+    });
+  } else {
+    console.error("Element with id 'copyLink' not found.");
+  }
 });
