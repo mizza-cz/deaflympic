66e94d1940b6e0aefbb2f9406995cb21549581dd
mizza-cz <mizza.cz666@gmail.com>
2024-01-04 08:19:57 +0100

    fix share social

--- src/components/article-section/article-section.js
+++ src/components/article-section/article-section.js
@@ -1,18 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var copyLinkButton = document.getElementById("copyLink");
+setShareLinks();
 
-  if (copyLinkButton) {
-    copyLinkButton.addEventListener("click", function () {
-      var linkToCopy = this.getAttribute("data-link");
-      var textarea = document.createElement("textarea");
-      textarea.value = linkToCopy;
-      document.body.appendChild(textarea);
-      textarea.select();
-      textarea.setSelectionRange(0, 99999);
-      document.execCommand("copy");
-      document.body.removeChild(textarea);
-    });
-  } else {
-    console.error("Element with id 'copyLink' not found.");
-  }
-});
+function socialWindow(url) {
+  var left = (screen.width - 570) / 2;
+  var top = (screen.height - 570) / 2;
+  var params =
+    "menubar=no,toolbar=no,status=no,width=570,height=570,top=" +
+    top +
+    ",left=" +
+    left;
+  // Setting 'params' to an empty string will launch
+  // content in a new tab or window rather than a pop-up.
+  // params = "";
+  window.open(url, "NewWindow", params);
+}
+
+function setShareLinks() {
+  var pageUrl = encodeURIComponent(document.URL);
+  var tweet = encodeURIComponent(
+    $("meta[property='og:description']").attr("content")
+  );
+
+  $(".social-share.facebook").on("click", function () {
+    url = "https://www.facebook.com/sharer/sharer.php?u=" + pageUrl;
+    socialWindow(url);
+  });
+
+  $(".social-share.twitter").on("click", function () {
+    url = "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" + tweet;
+    socialWindow(url);
+  });
+
+  $(".social-share.linkedin").on("click", function () {
+    url = "https://www.linkedin.com/shareArticle?mini=true&url=" + pageUrl;
+    socialWindow(url);
+  });
+}
+
