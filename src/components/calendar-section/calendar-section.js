1fed17330c8857c726b5de26bb3c2f665c8249d7
mizza-cz <mizza.cz666@gmail.com>
2025-07-11 09:22:13 +0200

    add pagination calendar

--- src/components/calendar-section/calendar-section.js
+++ src/components/calendar-section/calendar-section.js
@@ -1,47 +1,3 @@
-// $(function () {
-//   $(".date-picker__calendar").datepicker({
-//     dateFormat: "dd/mm/yy",
-//     firstDay: 1,
-//   });
-
-//   $(document).on("click", function (e) {
-//     // Игнорируем клики по календарю и стрелкам переключения месяцев
-//     if (
-//       !$(e.target).closest(".date-picker").length ||
-//       $(e.target).closest(".ui-datepicker-prev, .ui-datepicker-next").length
-//     ) {
-//       return;
-//     }
-
-//     // Закрываем календарь, если клик вне date-picker
-//     if (!$(e.target).closest(".date-picker").length) {
-//       $(".date-picker").removeClass("open");
-//     }
-//   });
-
-//   $(document).on("click", ".date-picker .input", function (e) {
-//     var $me = $(this),
-//       $parent = $me.parents(".date-picker");
-//     $parent.toggleClass("open");
-//   });
-
-//   $(".date-picker__calendar").on("change", function () {
-//     var $me = $(this),
-//       $selected = $me.val(),
-//       $parent = $me.parents(".date-picker");
-//     $parent.find(".result").children("span").html($selected);
-//     $parent.removeClass("open"); // Сворачиваем при выборе даты
-//   });
-
-//   // Отключаем автоматическое закрытие при переключении месяцев
-//   $(document).on(
-//     "click",
-//     ".ui-datepicker-prev, .ui-datepicker-next",
-//     function (e) {
-//       e.stopPropagation(); // Предотвращаем событие клика
-//     }
-//   );
-// });
 $(function () {
   // Устанавливаем чешскую локализацию
   $.datepicker.regional["cs"] = {
@@ -145,3 +101,59 @@ $(function () {
     }
   );
 });
+
+/// pagination
+
+(function () {
+  const blogGrid = document.querySelector("#calendar-list");
+  const loadMoreBtn = document.querySelector(".calendar-load-more");
+
+  if (!blogGrid || !loadMoreBtn || typeof newsSource === "undefined") return;
+
+  const checkNewsCount = () => {
+    const items = blogGrid.querySelectorAll(".calendarItem");
+  };
+
+  checkNewsCount();
+
+  loadMoreBtn.addEventListener("click", async () => {
+    const loader = document.createElement("div");
+    loader.classList.add("loader");
+    loadMoreBtn.prepend(loader);
+
+    const currentPage = parseInt(loadMoreBtn.dataset.page ?? "2", 10);
+
+    const url = newsSource.includes("?")
+      ? `${newsSource}&pageNumber=${currentPage}`
+      : `${newsSource}?pageNumber=${currentPage}`;
+
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: { Accept: "application/json" },
+        credentials: "same-origin",
+      });
+
+      loader.remove();
+
+      if (!response.ok) return;
+
+      const data = await response.json();
+
+      if (data.html) {
+        blogGrid.innerHTML += data.html;
+      }
+
+      checkNewsCount();
+
+      if (data.loadMore === false) {
+        loadMoreBtn.style.display = "none";
+      } else {
+        loadMoreBtn.dataset.page = currentPage + 1;
+      }
+    } catch (error) {
+      console.error("Chyba při načítání dat:", error);
+      loader.remove();
+    }
+  });
+})();
